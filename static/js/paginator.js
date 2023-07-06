722eeadf53bd998c8413d68299b5b78128b6c217
mjthepollo <mjthepollo@gmail.com>
2023-07-06 17:24:35 +0900

    Now Paginator becomes convinient and has a nice code

--- static/js/paginator.js
+++ static/js/paginator.js
@@ -1,29 +1,11 @@
-const pageNumberInputBox = document.querySelector("#page_number_input_box");
-const pageNumberButton = document.querySelector("#page_number_button");
-const pageNumberMoveButton = document.querySelector("#page_number_move_button");
-const pageNumberCancelButton = document.querySelector("#page_number_cancel_button");
 const pageNumberInput = document.querySelector("input#page_number_input");
-const urlWithoutPage = pageNumberInputBox.dataset.url_without_page;
+const urlWithoutPage = pageNumberInput.dataset.url_without_page;
 
-function handlePageNumberButtonClick(e) {
-  pageNumberInputBox.classList.remove("inactive");
-  pageNumberInput.focus();
-  pageNumberInput.select();
-}
-function handlePageNumberMoveButtonClick(e) {
+console.log(urlWithoutPage);
+function goToPage(e) {
   const pageNumber = pageNumberInput.value;
   const url = urlWithoutPage + pageNumber;
+  console.log(pageNumber, url);
   window.location.href = url;
 }
-
-function handlePageNumberCancelButtonClick(e) {
-  pageNumberInputBox.classList.add("inactive");
-}
-
-pageNumberButton.addEventListener("click", handlePageNumberButtonClick);
-pageNumberMoveButton.addEventListener("click", handlePageNumberMoveButtonClick);
-pageNumberCancelButton.addEventListener("click", handlePageNumberCancelButtonClick);
-
-function test() {
-  alert("Enter Key 입력 감지 \n함수 실행.");
-}
+// goToPage is called when the user presses enter in the input field
