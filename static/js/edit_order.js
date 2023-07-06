0ab030a44ae8b374e58e4e654eeec65e17d34e90
mjthepollo <mjthepollo@gmail.com>
2023-07-06 16:15:56 +0900

    Now edit_order js is done

--- static/js/edit_order.js
+++ static/js/edit_order.js
@@ -6,17 +6,49 @@ import {getIdOfField} from "./modal/modal.js";
 const totalNumberOfEditOrderForms = document.querySelectorAll(".edit_order_form").length;
 const totlaNumberOfEditOrderSet = totalNumberOfEditOrderForms / 4;
 
-function setChargeAmountDataFactory(paymentData, wageAmountInput, componentAmountInput) {
-  return function () {};
+function callEveryArgumentsFactory() {
+  const functions = arguments;
+  return function () {
+    for (let i = 0; i < functions.length; i++) {
+      functions[i]();
+    }
+  };
 }
-function setPyamentDataFactory(paymentData, wageAmountInput, componentAmountInput) {
-  return function () {};
+function setPaymentDataFactory(paymentData, faultRatioInput, indemnityAmountInput, refundAmountInput) {
+  return function () {
+    const faultRatio = parseInt(faultRatioInput.value);
+    const indemnityAmount = parseInt(indemnityAmountInput.value);
+    const refundAmount = parseInt(refundAmountInput.value);
+    paymentData.dataset.fault_ratio = isNaN(faultRatio) ? 100 : faultRatio;
+    paymentData.dataset.indemnity_amount = isNaN(indemnityAmount) ? 0 : indemnityAmount;
+    paymentData.dataset.refund_amount = isNaN(refundAmount) ? 0 : refundAmount;
+  };
+}
+
+function setChargeAmountDataFactory(chargeAmountData, wageAmountInput, componentAmountInput, paymentData) {
+  return function () {
+    const wageAmount = parseInt(wageAmountInput.value);
+    const componentAmount = parseInt(componentAmountInput.value);
+    const repairAmount = wageAmount + componentAmount;
+    const faultRatio = parseInt(paymentData.dataset.fault_ratio);
+    const chargableAmount = (repairAmount * faultRatio * 1.1) / 100;
+    const indemnityAmount = parseInt(paymentData.dataset.indemnity_amount);
+    const refundAmount = parseInt(paymentData.dataset.refund_amount);
+    let chargeAmount = chargableAmount - indemnityAmount + refundAmount;
+    if (isNaN(chargableAmount)) {
+      chargeAmountData.dataset.charge_amount = "";
+    } else {
+      chargeAmount = chargeAmount > 0 ? chargeAmount : 0;
+      chargeAmountData.dataset.charge_amount = chargeAmount;
+    }
+  };
 }
 
 for (let i = 0; i < totlaNumberOfEditOrderSet; i++) {
   const faultRatioInput = document.querySelector(`#${getIdOfField("order", "fault_ratio", i)}`);
   const indemnityAmountInput = document.querySelector(`#${getIdOfField("payment", "indemnity_amount", i)}`);
   const discountAmountInput = document.querySelector(`#${getIdOfField("payment", "discount_amount", i)}`);
+  const refundAmountInput = document.querySelector(`#${getIdOfField("payment", "refund_amount", i)}`);
   const settlementAmountInfo = document.querySelectorAll(".settlement_amount_info")[i];
   const wageAmountInput = document.querySelector(`#${getIdOfField("charge", "wage_amount", i)}`);
   const componentAmountInput = document.querySelector(`#${getIdOfField("charge", "component_amount", i)}`);
@@ -30,11 +62,9 @@ for (let i = 0; i < totlaNumberOfEditOrderSet; i++) {
   const paymentRateInfo = document.querySelectorAll(".payment_rate_info")[i];
   const curRateInfo = document.querySelectorAll(".cut_rate_info")[i];
 
-  const setSettlementAmount = setSettlementAmountInfoFactory({}, settlementAmountInfo, indemnityAmountInput, discountAmountInput);
-  indemnityAmountInput.addEventListener("input", setSettlementAmount);
-  discountAmountInput.addEventListener("input", setSettlementAmount);
-  setSettlementAmount();
-
+  const setSettlementAmount = setSettlementAmountInfoFactory(settlementAmountInfo, indemnityAmountInput, discountAmountInput);
+  const setPaymentData = setPaymentDataFactory(paymentData, faultRatioInput, indemnityAmountInput, refundAmountInput);
+  const setChargeAmountData = setChargeAmountDataFactory(chargeAmountData, wageAmountInput, componentAmountInput, paymentData);
   const setChargeInfo = setChargeInfoFactory(
     wageAmountInput,
     componentAmountInput,
@@ -44,11 +74,15 @@ for (let i = 0; i < totlaNumberOfEditOrderSet; i++) {
     chargableAmountInfo,
     chargeAmountInfo
   );
-  wageAmountInput.addEventListener("input", setChargeInfo);
-  componentAmountInput.addEventListener("input", setChargeInfo);
-  setChargeInfo();
-
   const setRateInfo = setRateInfoFactory(chargeAmountData, depositAmountInput, paymentRateInfo, curRateInfo);
-  depositAmountInput.addEventListener("input", setRateInfo);
-  setRateInfo();
+
+  const handleAll = callEveryArgumentsFactory(setPaymentData, setChargeAmountData, setChargeInfo, setRateInfo, setSettlementAmount);
+  faultRatioInput.addEventListener("input", handleAll);
+  indemnityAmountInput.addEventListener("input", handleAll);
+  discountAmountInput.addEventListener("input", handleAll);
+  refundAmountInput.addEventListener("input", handleAll);
+  wageAmountInput.addEventListener("input", handleAll);
+  componentAmountInput.addEventListener("input", handleAll);
+  depositAmountInput.addEventListener("input", handleAll);
+  handleAll();
 }
